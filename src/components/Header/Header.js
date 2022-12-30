bd741402de896e503733d2c2831aa44e590179b2
Oscar Carantón <oscarcaranton@MacBook-Pro-de-Oscar.local>
2022-12-30 07:01:30 -0500

    bugfix home, adjust arrow animation icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Header.module.scss';
 
 
-const Header = ({ title, username }) => (
-  <div className={styles.container} data-testid="Header">
-    <div className={styles.logo}>
-      <img src='prutec/TuPruTec.svg' className="App-logo" alt={title} />
+const Header = ({ title, username }) => {
+  const [arrowActive, setArrowActive] = useState(false);
+
+  const changeArrow = () => {
+    console.log({ arrowActive })
+    setArrowActive(!arrowActive);
+  }
+
+  return (
+    <div className={styles.container} data-testid="Header">
+      <div className={styles.logo}>
+        <img src='prutec/TuPruTec.svg' className="App-logo" alt={title} />
+      </div>
+      <div className={styles.username}>
+        <span>{username}</span>
+        <img src='prutec/ArrowDown.svg'
+          onClick={() => changeArrow()}
+          className={arrowActive ? styles.arrowActive : styles.arrow }
+          alt="usernameIcon" />
+      </div>
     </div>
-    <div className={styles.username}>
-      <span>{username}</span>
-      <img src='prutec/ArrowDown.svg' className="Arrow-icon" alt="usernameIcon" />
-    </div>
-  </div>
-);
+  );
+}
 
 Header.propTypes = {
   title: PropTypes.string,
